15981d3c9079d8532f031593ebf538ef0f78c5bb
김기태 <aqz103@naver.com>
2026-01-30 15:49:57 +0900

    feat: 주문페이지

--- store-web/src/pages/Home.jsx
+++ store-web/src/pages/Home.jsx
@@ -1,21 +1,36 @@
+import { useNavigate } from 'react-router-dom';
 import './css/MenuButtons.css'
 
+
 export default function MenuButtons() {
+  const navigate = useNavigate();
+
   return (
     <div className="menu-container">
       {/* 상단 4개 버튼 */}
       <div className="menu-top">
-        <button className="menu-btn">🍔🍟🥤<span>햄버거 세트</span></button>
-        <button className="menu-btn">🍔<span>햄버거</span></button>
-        <button className="menu-btn">🍟<span>사이드</span></button>
-        <button className="menu-btn">🥤<span>음료</span></button>
-      </div>
+        <button className="menu-btn" onClick={() => navigate("/menu/set")}>
+          🍔🍟🥤 <span>햄버거 세트</span>
+        </button>
 
+        <button className="menu-btn" onClick={() => navigate("/menu/burger")}>
+          🍔 <span>햄버거</span>
+        </button>
+
+        <button className="menu-btn" onClick={() => navigate("/menu/side")}>
+          🍟 <span>사이드</span>
+        </button>
+
+        <button className="menu-btn" onClick={() => navigate("/menu/drink")}>
+          🥤 <span>음료</span>
+        </button>
+      </div>
       {/* 하단 2개 버튼 */}
       <div className="menu-bottom">
         <button className="menu-btn admin">⚙️<span>관리자</span></button>
         <button className="menu-btn close">🔒<span>마감</span></button>
       </div>
     </div>
+
   )
 }
--- store-web/src/router/AppRouter.jsx
+++ store-web/src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home.jsx'
 import Login from '../pages/Login.jsx'
 import NotFound from '../pages/NotFound.jsx'
+import MenuPage from '../pages/Menu/MenuPage.jsx'
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/**" element={<Login />} />
+
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
+
+      <Route path="/menu/:category" element={<MenuPage />} />
+
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* menu/:category */}
+      {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
   )
 }
